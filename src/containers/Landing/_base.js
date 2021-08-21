2acf2fd2e08a6a35266b126303afe1079d1c38b8
Saksham <sakshamjohri@gmail.com>
2021-08-21 18:04:11 +0530

    worked on main page

--- src/containers/Landing/_base.js
+++ src/containers/Landing/_base.js
@@ -1,5 +1,7 @@
 import googleIcon from '../../assets/icons/google.png';
+import youtubeIcon from '../../assets/icons/youtube.png';
 import googleLogo from '../../assets/logos/google.png';
+import youtubeLogo from '../../assets/logos/youtube.png';
 
 export const engineList = [
   {
@@ -8,5 +10,12 @@ export const engineList = [
     url: 'https://www.google.com/search?q=',
     icon: googleIcon,
     logo: googleLogo,
+  },
+  {
+    id: 2,
+    name: 'YouTube',
+    url: 'https://www.youtube.com/results?search_query=',
+    icon: youtubeIcon,
+    logo: youtubeLogo,
   },
 ];
--- src/containers/Landing/index.jsx
+++ src/containers/Landing/index.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from '../../components/SIdeBar';
+import Search from '../../components/Search';
 import { engineList } from './_base';
 import './style.scss';
 
 const Landing = () => {
+  const [selectedEngine, setSelectedEngine] = useState(engineList?.[0]);
+
   return (
     <div className='landing'>
-      <Sidebar list={engineList} />
+      <Sidebar
+        list={engineList}
+        selected={selectedEngine}
+        setSelected={setSelectedEngine}
+      />
 
-      <div className='search-container'>serch</div>
+      <Search item={selectedEngine} />
     </div>
   );
 };
