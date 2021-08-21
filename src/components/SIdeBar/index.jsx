17d9f603959ebfe61458958f9c0b128729f302eb
Saksham <sakshamjohri@gmail.com>
2021-08-21 18:03:45 +0530

    improved sidebar

--- src/components/SIdeBar/index.jsx
+++ src/components/SIdeBar/index.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import './style.scss';
 
-const Sidebar = ({ list }) => {
+const Sidebar = ({ list = [], selected = {}, setSelected = () => {} }) => {
   return (
     <div className='sidebar'>
       {list?.map((item, index) => {
+        const { name = '', icon = null } = item || {};
+
         return (
-          <div key={index} className='sidebar-item'>
-            <img src={item?.icon} alt='' className='img' />
+          <div
+            key={index}
+            className={`sidebar-item ${item === selected ? 'selected' : ''}`}
+            onClick={() => setSelected(item)}
+            title={name}
+          >
+            <img src={icon} alt='' className='img' />
           </div>
         );
       })}
