ecaf2e889135066de19d4693a8a638b829022630
Saksham <sakshamjohri@gmail.com>
2021-08-22 17:07:52 +0530

    added some search engines

--- src/containers/Landing/_base.js
+++ src/containers/Landing/_base.js
@@ -1,7 +1,14 @@
 import googleIcon from '../../assets/icons/google.png';
 import youtubeIcon from '../../assets/icons/youtube.png';
+import stackoverflowIcon from '../../assets/icons/stackoverflow.png';
+import wikipediaIcon from '../../assets/icons/wikipedia.png';
+import githubIcon from '../../assets/icons/github.png';
+
 import googleLogo from '../../assets/logos/google.png';
 import youtubeLogo from '../../assets/logos/youtube.png';
+import stackoverflowLogo from '../../assets/logos/stackoverflow.png';
+import wikipediaLogo from '../../assets/logos/wikipedia.png';
+import githubLogo from '../../assets/logos/github.png';
 
 export const engineList = [
   {
@@ -17,5 +24,26 @@ export const engineList = [
     url: 'https://www.youtube.com/results?search_query=',
     icon: youtubeIcon,
     logo: youtubeLogo,
+  },
+  {
+    id: 3,
+    name: 'StackOverflow',
+    url: 'https://stackoverflow.com/search?q=',
+    icon: stackoverflowIcon,
+    logo: stackoverflowLogo,
+  },
+  {
+    id: 4,
+    name: 'Wikipedia',
+    url: 'https://en.wikipedia.org/wiki/Special:Search?search=',
+    icon: wikipediaIcon,
+    logo: wikipediaLogo,
+  },
+  {
+    id: 5,
+    name: 'Github',
+    url: 'https://github.com/search?q=',
+    icon: githubIcon,
+    logo: githubLogo,
   },
 ];
